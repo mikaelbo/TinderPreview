05189d6df0f8645c981b59d1b7ba5935b191e3cd
mikaelbo <mbo@mbo42.com>
2017-09-26 22:26:12 +0800

    Adjusted font logic for latest Tinder version

--- Source/UIFont+MBTNDRFont.m
+++ Source/UIFont+MBTNDRFont.m
@@ -3,22 +3,26 @@
 @implementation UIFont(MBTNDRFont)
 
 + (UIFont *)defaultFontOfSize:(CGFloat)size {
-	UIFont *font = [UIFont fontWithName:@"ProximaNovaSoft-Regular" size:size];
-	return font ? : [UIFont systemFontOfSize:size];
+    UIFont *font = [UIFont fontWithName:@"ProximaNovaSoft-Regular" size:size];
+    if (!font) { font = [UIFont fontWithName:@"ProximaNova-Regular" size:size]; }
+    return font ? : [UIFont systemFontOfSize:size];
 }
 
 + (UIFont *)boldFontOfSize:(CGFloat)size {
     UIFont *font = [UIFont fontWithName:@"ProximaNovaSoft-Bold" size:size];
-    return font ? : [UIFont boldFontOfSize:size];
+    if (!font) { font = [UIFont fontWithName:@"ProximaNova-Bold" size:size]; }
+    return font ? : [UIFont boldSystemFontOfSize:size];
 }
 
 + (UIFont *)semiBoldFontOfSize:(CGFloat)size {
     UIFont *font = [UIFont fontWithName:@"ProximaNovaSoft-Semibold" size:size];
+    if (!font) { font = [UIFont fontWithName:@"ProximaNova-Semibold" size:size]; }
     return font ? : [self boldFontOfSize:size];
 }
 
 + (UIFont *)mediumFontOfSize:(CGFloat)size {
     UIFont *font = [UIFont fontWithName:@"ProximaNovaSoft-Medium" size:size];
+    if (!font) { font = [UIFont fontWithName:@"ProximaNova-Semibold" size:size]; }
     return font ? : [self defaultFontOfSize:size];
 }
 
